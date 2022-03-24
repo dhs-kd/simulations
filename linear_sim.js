33ffb9c9b67547d8ffa2e3657974d42db30c2e4a
Mustafa Alwash <92398708+Mustafa-Alwash@users.noreply.github.com>
2022-03-24 19:22:10 +0300

    Add files via upload

---
+++ linear_sim.js
@@ -0,0 +1,31 @@
+var vel =  2;
+var acc = 1;
+var x = 0
+
+function setup(){
+    createCanvas(400,500)
+}
+
+
+function draw(){
+    background(0)
+    //position
+    var y =  height/2
+     
+    //moving
+    function move(dt){
+        x += vel * dt
+        vel += acc * dt
+    }
+
+    //draw an object
+    fill('yellow')
+    rect(x ,y, 55, 60)
+
+    move(1)
+
+    if (x > width){
+        x = 0
+    }
+}
+
